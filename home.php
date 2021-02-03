9db765f3e27104ff9d7b781500e62835cf8699fb
carlosjonas <carlosjonasfon@gmail.com>
2021-02-03 20:36:58 -0300

    Criação da minha pagina home e inserção do layout da tabela do crud  com as colunas de nome,rg,cpf,email,endereco,telefone1 e telefone2 , criação do bando de dados crud com os mesmos campos

---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD</title>
+</head>
+<body>
+	<h1>CRUD - Aprendiz de Back End</h1>
+
+	<table>
+		<thead>
+			<tr>
+				<th>Nome:</th>
+				<th>RG:</th>
+				<th>CPF:</th>
+				<th>Email:</th>
+				<th>Endereço:</th>
+				<th>Telefone 1:</th>
+				<th>Telefone 2:</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Jonas</td>
+				<td>078736551342</td>
+				<td>20086085870</td>
+				<td>carlosjonasfon@gmail.com</td>
+				<td>Rua Santa Inês, 1532</td>
+				<td>(85) 988873961</td>
+				<td>(85) 985430012</td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+</html>
