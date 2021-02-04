d3fbd9d2c1a39ba8d141a997d47864d85a1fb6e3
carlosjonas <carlosjonasfon@gmail.com>
2021-02-03 21:49:35 -0300

    Criando a conexao com o meu banco de dados

--- acao/conexao.php
+++ acao/conexao.php
@@ -1,6 +1,12 @@
 <?php 
+// Criando as variáveis para a conexão com o banco de dados
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "crud";
 
-
+// Criando a conexão com o banco 
+$conexao = mysqli_connect($servername, $username, $password, $database);
 
  ?>
